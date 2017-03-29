a2c7617bb66f9b0e97118b680e64ce6ec319ab85
Brad Schmitt <bschmitt@gmail.com>
2017-03-29 18:39:23 -0500

    Added additional fields

--- tsi-remexcel.py
+++ tsi-remexcel.py
@@ -3,16 +3,15 @@ import json
 import logging
 import sys
 import time
+import datetime
 import concurrent.futures as cf
 from requests_futures.sessions import FuturesSession
 
-with open('param.json') as json_data:
+with open('param_secret.json') as json_data:
     parms = json.load(json_data)
 
-threeyears = 94608000
-
 def getCSVHeader():
-    mappings={}
+    mappings = {}
     f = open(parms["file"])
     reader = csv.reader(f)
     header = reader.__next__()
@@ -22,24 +21,27 @@ def getCSVHeader():
 
     return mappings
 
-def getItem(mappings,value):
 
+def getItem(mappings, value):
     text = ""
-    #print(mappings)
-    #print(value)
-    for k,v in mappings.items():
-        #print("value: " + str(value) + " = v: " + str(v))
-        if(int(value) == int(v)):
+    # print(mappings)
+    # print(value)
+    for k, v in mappings.items():
+        # print("value: " + str(value) + " = v: " + str(v))
+        if (int(value) == int(v)):
             text = k
-            #print(text)
+            # print(text)
 
     return text
 
-def createEventJSON():
+def convertTS(ts):
+    return datetime.datetime.fromtimestamp(int(ts)).strftime('%m-%d-%Y %H:%M:%S')
 
-    eventList=[]
+
+def createEventJSON():
+    eventList = []
     header = getCSVHeader()
-    f = open(parms["file"],encoding='Windows-1252')
+    f = open(parms["file"], encoding='Windows-1252')
     reader = csv.reader(f)
     reader.__next__()
 
@@ -50,22 +52,32 @@ def createEventJSON():
                 "sender": parms["sourcesender"],
                 "fingerprintFields": parms["fingerprintfields"],
                 "title": events[header["DESCRIPTION"]],
-                "status": getItem(parms["status"],events[header["STATUS"]]),
-                "createdAt": int(events[header["SUBMIT_DATE"]]) + threeyears,
+                "status": getItem(parms["status"], events[header["STATUS"]]),
+                "createdAt": int(events[header["SUBMIT_DATE"]])+parms['shift'],
                 "eventClass": "Incident",
                 "properties": {
-                    "urgency": getItem(parms["urgency"],events[header["URGENCY"]]),
-                    "impact": getItem(parms["impact"],events[header["IMPACT"]]),
-                    "incident_id": events[header["INCIDENT_NUMBER"]],
-                    "owner_group": events[header["OWNER_GROUP"]],
+                    "app_id": parms["app_id"],
                     "assigned_group": events[header["ASSIGNED_GROUP"]],
-                    "organization": events[header["ORGANIZATION"]],
-                    "closed_date": events[header["CLOSED_DATE"]],
-                    "city": events[header["CITY"]],
-                    "site": events[header["SITE"]],
-                    "company": events[header["COMPANY"]],
+                    "assigned_support_company": events[header["ASSIGNED_SUPPORT_COMPANY"]],
+                    "assigned_support_org": events[header["ASSIGNED_SUPPORT_ORGANIZATION"]],
                     "assignee": events[header["ASSIGNEE"]],
-                    "app_id": parms["app_id"]
+                    "city": events[header["CITY"]],
+                    "closed_date": convertTS(int(events[header["CLOSED_DATE"]])+parms['shift']),
+                    "company": events[header["COMPANY"]],
+                    "country": events[header["COUNTRY"]],
+                    "department": events[header["DEPARTMENT"]],
+                    "impact": getItem(parms["impact"], events[header["IMPACT"]]),
+                    "last_modified_date": convertTS(int(events[header["LAST_MODIFIED_DATE"]])+parms['shift']),
+                    "last_resolved_date": convertTS(int(events[header["LAST_RESOLVED_DATE"]])+parms['shift']),
+                    "incident_id": events[header["INCIDENT_NUMBER"]],
+                    "organization": events[header["ORGANIZATION"]],
+                    "owner_group": events[header["OWNER_GROUP"]],
+                    "reported_date": convertTS(int(events[header["REPORTED_DATE"]])+parms['shift']),
+                    "resolution_category": events[header["RESOLUTION_CATEGORY"]],
+                    "site": events[header["SITE"]],
+                    "state_province": events[header["STATE_PROVINCE"]],
+                    "submit_date": convertTS(int(events[header["SUBMIT_DATE"]])+parms['shift']),
+                    "urgency": getItem(parms["urgency"], events[header["URGENCY"]]),
                 },
                 "tags": [parms["app_id"]]
             }
@@ -76,8 +88,8 @@ def createEventJSON():
 
     return eventList
 
-def sendAsyncEvents(events):
 
+def sendAsyncEvents(events):
     logging.basicConfig(
         stream=sys.stderr, level=logging.INFO,
         format='%(relativeCreated)s %(message)s',
@@ -88,15 +100,27 @@ def sendAsyncEvents(events):
 
     logging.info('start')
 
-    counter=0
-    for event in events:
-        while counter < parms['chunksize']:
-            future = session.post(parms['url'],data=event,headers=parms['headers'],auth=(parms['email'],parms['apikey']))
-            futures[future] = event
-            print(event)
-            counter+=1
-        counter=0
-        time.sleep(parms['sleeptime'])
+    bufcounter = 0
+    eventcounter = 0
+    while (eventcounter < len(events)):
+        if (bufcounter == parms['chunksize']):
+            print('sleeping...')
+            time.sleep(parms['sleeptime'])
+            bufcounter = 0
+        else:
+            future = session.post(parms['url'], data=events[eventcounter], headers=parms['headers'], auth=(parms['email'], parms['apikey']))
+            futures[future] = events[eventcounter]
+            print(str(eventcounter) + ": " + events[eventcounter])
+            eventcounter += 1
+            bufcounter += 1
+
+    """ for event in events:
+        future = session.post(parms['url'],data=event,headers=parms['headers'],auth=(parms['email'],parms['apikey']))
+        futures[future] = event
+        print(event)
+        counter+=1
+
+        time.sleep(parms['sleeptime']) """
 
     for future in cf.as_completed(futures):
         res = future.result()
@@ -109,6 +133,6 @@ def sendAsyncEvents(events):
 
     logging.info('done!')
 
+
 events = createEventJSON()
 sendAsyncEvents(events)
-
